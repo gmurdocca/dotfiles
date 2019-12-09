4fafa5ac2c56144168d4da8f6c3c77ab1a8dd6c9
George Murdocca <george@linuxdojo.com>
2019-12-09 13:50:25 +1100

    set autoreload 2 enabled by default in ipython

--- _ipython/profile_default/ipython_config.py
+++ _ipython/profile_default/ipython_config.py
@@ -1,4 +1,8 @@
-c = get_config()
-c.InteractiveShellApp.extensions = [
-    'powerline.bindings.ipython.post_0_11'
-]
+#from powerline.bindings.ipython.since_5 import PowerlinePrompts
+#c = get_config()
+#c.TerminalInteractiveShell.simple_prompt = False
+#c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+
+# Autoreload imports
+c.InteractiveShellApp.extensions = ['autoreload']
+c.TerminalIPythonApp.exec_lines = ['%autoreload 2']
